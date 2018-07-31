ed67223c8ac0f9c1c6b69b0aac69c55d6b66e201
JP Lynch <JPLynch35@gmail.com>
2018-07-31 09:19:18 -0600

    add relationship tests and relationships for grenre, song, and song_genre

---
+++ spec/models/genre_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Genre, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:song_genres) }
+    it { should have_many(:songs).through(:song_genres) }
+  end
+
+  describe 'instance methods' do
+    
+  end
+
+  describe 'class methods' do
+    
+  end
+end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -8,6 +8,8 @@ describe Song, type: :model do
   end
 
   describe 'relationships' do
-    it{should belong_to(:artist)}
+    it { should belong_to(:artist)}
+    it { should have_many(:song_genres)}
+    it { should have_many(:genres).through(:song_genres) }
   end
 end
